bf11ef2894ecad55dd1803324b9f6e7f5b0b0138
Maryhelen Albuquerque bastos <malbuque@c1r14s1.42lisboa.com>
2021-11-08 23:41:22 +0000

    add memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbuque <malbuque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 23:00:28 by malbuque          #+#    #+#             */
+/*   Updated: 2021/11/08 23:26:59 by malbuque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	string x;
+	
+	x = dst
+	while (n--)
+	{
+		dst = src
+	}
+	return(x);
+}
