46531b7140e4425a2e415931c3656a67d23908d9
Maryhelen Albuquerque bastos <malbuque@c1r11s1.42lisboa.com>
2021-11-09 22:37:24 +0000

    add 3 versao

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,15 +6,15 @@
 /*   By: malbuque <malbuque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 21:41:19 by malbuque          #+#    #+#             */
-/*   Updated: 2021/11/09 22:36:18 by malbuque         ###   ########.fr       */
+/*   Updated: 2021/11/09 22:37:05 by malbuque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *memmove(void *dst, const void *src, size_t len)
+void	*memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char *original;
+	unsigned char	*original;
 
 	while (len--)
 		original[len] = ((unsigned char *)src)[len];
